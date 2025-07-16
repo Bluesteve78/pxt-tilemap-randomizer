08df33c7498e5e37b00e8fdcc6922664c98f30f4
Bluesteve78 <caleb.altier45@masonohioschools.com>
2025-07-16 11:25:33 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-namespace Randomizer {
+//% weight=100 color=#0077cc icon="\uf278"
+namespace TilemapRandomizer {
+    //% block
+    export function test(): void {
+        game.splash("Tilemap Randomizer is working!")
+    }
+}
+
